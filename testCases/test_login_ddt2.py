18fb6e53e8725c2423329c35ccfb572768759e15
farihasazid8 <fariha.sazid@klovercloud.com>
2022-11-23 11:10:55 +0600

    few updates

--- testCases/test_login_ddt2.py
+++ testCases/test_login_ddt2.py
@@ -7,19 +7,20 @@ from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
 import raf_practice.logs.customolog.custom_logger as cl
 from utilities import XLUtils
+from utilities.readProperties import ReadConfig
+import time
 
 
 class Test_002_Login:
     baseURL = ReadConfig.getApplicationURL()
-    # logger = LogGen.loggen()
     logger = cl.customLogger(logging.DEBUG)
-    path = "C:/Users/shabr/PycharmProjects/ClusterTest/TestData/Login_data.xlsx"
+    path = "C:/Users/shabr/PycharmProjects/ClusterTest/TestData/Login_data_two.xlsx"
 
     @pytest.mark.sanity
     @pytest.mark.regression
     def test_login(self, setup):
-        self.logger.info("**********Test_002_Login*********")
-        self.logger.info("****Verifying Login DDT Test****")
+        self.logger.info("**********Test_003_Login*********")
+        self.logger.info("****Verifying Login DDT Test2****")
         self.driver = setup
         self.driver.get(self.baseURL)
         self.driver.implicitly_wait(20)
@@ -28,18 +29,16 @@ class Test_002_Login:
         self.rows = XLUtils.getRowCount(self.path, "Sheet1")
         print("Number of Rows i a Excel: ", self.rows)
         lst_status = []
-
         for r in range(2, self.rows + 1):
-            self.username = XLUtils.readData(self.path, "Sheet1", r, 1)
-            self.password = XLUtils.readData(self.path, "Sheet1", r, 2)
-            self.exp = XLUtils.readData(self.path, "Sheet1", r, 3)
+            self.user = XLUtils.readData(self.path, 'Sheet1', r, 1)
+            self.password = XLUtils.readData(self.path, 'Sheet1', r, 2)
+            self.exp = XLUtils.readData(self.path, 'Sheet1', r, 3)
 
-            self.lp.setUserName(self.username)
-            time.sleep(1)
+            self.lp.setUserName(self.user)
             self.lp.setPassword(self.password)
-            time.sleep(1)
             self.lp.clickLogin()
-            time.sleep(6)
+            time.sleep(5)
+
             act_title = self.driver.title
             exp_title = "KloverCloud | Dashboard"
 
@@ -49,14 +48,16 @@ class Test_002_Login:
                     self.lp.ClickProfile()
                     time.sleep(2)
                     self.lp.clickLogout()
-                    time.sleep(4)
+                    time.sleep(3)
+                    # self.lp.clickLogout();
                     lst_status.append("Pass")
                 elif self.exp == 'Fail':
                     self.logger.info("**** failed ****")
                     self.lp.ClickProfile()
                     time.sleep(2)
                     self.lp.clickLogout()
-                    time.sleep(4)
+                    time.sleep(3)
+                    # self.lp.clickLogout();
                     lst_status.append("Fail")
 
             elif act_title != exp_title:
@@ -67,6 +68,7 @@ class Test_002_Login:
                     self.logger.info("**** passed ****")
                     lst_status.append("Pass")
             print(lst_status)
+        self.driver.refresh()
         if "Fail" not in lst_status:
             self.logger.info("******* DDT Login test passed **********")
             self.driver.close()
@@ -77,4 +79,56 @@ class Test_002_Login:
             assert False
 
         self.logger.info("******* End of Login DDT Test **********")
-        self.logger.info("**************** Completed  TC_LoginDDT_002 ************* ")
+        self.logger.info("**************** Completed  TC_LoginDDT_002 ************* ");
+
+        # lst_status = []
+        # for r in range(2, self.rows + 1):
+        #     self.username = XLUtils.readData(self.path, "Sheet1", r, 1)
+        #     self.password = XLUtils.readData(self.path, "Sheet1", r, 2)
+        #     self.exp = XLUtils.readData(self.path, "Sheet1", r, 3)
+        #
+        #     self.lp.setUserName(self.username)
+        #     time.sleep(1)
+        #     self.lp.setPassword(self.password)
+        #     time.sleep(1)
+        #     self.lp.clickLogin()
+        #     time.sleep(6)
+        #     act_title = self.driver.title
+        #     exp_title = "KloverCloud | Dashboard"
+        #
+        #     if act_title == exp_title:
+        #         if self.exp == 'Pass':
+        #             self.logger.info("**** passed ****")
+        #             self.lp.ClickProfile()
+        #             time.sleep(2)
+        #             self.lp.clickLogout()
+        #             time.sleep(3)
+        #             lst_status.append("Pass")
+        #         elif self.exp == 'Fail':
+        #             self.logger.info("**** failed ****")
+        #             self.lp.ClickProfile()
+        #             time.sleep(2)
+        #             self.lp.clickLogout()
+        #             lst_status.append("Fail")
+        #             time.sleep(5)
+        #
+        #     elif act_title != exp_title:
+        #         if self.exp == 'Pass':
+        #             self.logger.info("**** failed ****")
+        #             lst_status.append("Fail")
+        #         elif self.exp == 'Fail':
+        #             self.logger.info("**** passed ****")
+        #             lst_status.append("Pass")
+        #             print(lst_status)
+        #
+        # if "Fail" not in lst_status:
+        #     self.logger.info("******* DDT Login test passed **********")
+        #     self.driver.close()
+        #     assert True
+        # else:
+        #     self.logger.error("******* DDT Login test failed **********")
+        #     self.driver.close()
+        #     assert False
+        #
+        # self.logger.info("******* End of Login DDT Test **********")
+        # self.logger.info("**************** Completed  TC_LoginDDT_002 ************* ")
