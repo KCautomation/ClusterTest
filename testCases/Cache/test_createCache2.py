737374dd5fb0bb60322499f5da06c380cf5d59d4
farihasazid8 <fariha.sazid@klovercloud.com>
2022-12-13 15:32:45 +0600

    few update

--- testCases/Cache/test_createCache2.py
+++ testCases/Cache/test_createCache2.py
@@ -34,9 +34,9 @@ class TestCreateCache:
     CC = CreateCaches()
 
     @pytest.mark.regression
-    def test_teamDefault(self, setup):
+    def test_TC001(self, setup):
 
-        self.logger.info("***************Try to Test_Create Cache *****************")
+        print("****************** Try to go create Cache based on Team: None & Version: 5.0.7   *********************")
         # self.logger.info("****Started Home page title test ****")
         self.driver = setup
         ss = SS(self.driver)
@@ -66,11 +66,170 @@ class TestCreateCache:
         except InvalidSessionIdException as e:
             print("InvalidSessionIdException", e)
 
-        print("****************** Try to go create Postgresql database *********************")
+        print("---------- Start create Cache based on Team: None & Version: 5.0.7  -------------")
         Team = cache.Team_None
         ServerName = "testCache0141"
         Password = "Qwer1234!!"
         Cache_Version = cache.Version_5_0_7
+        WebClientEmail = "test@gmail.com"
+        WebClientPassword = "Qwer1234!!"
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CC.cache(self, Team, ServerName, Password, Cache_Version, WebClientEmail, WebClientPassword)
+
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+    @pytest.mark.regression
+    def test_TC002(self, setup):
+
+        print("****************** Try to go create Cache based on Team: None & Version: 6.0.5  *********************")
+        # self.logger.info("****Started Home page title test ****")
+        self.driver = setup
+        ss = SS(self.driver)
+        cache = CreateCache(self.driver)
+
+        print("****************** Try to Test Cluster Login *********************")
+        try:
+            self.login.test_cluster_login(self)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("-------------------- Try to go create cache page ---------------")
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CF.go_createCachePage(self)
+            time.sleep(3)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("---------- Start create Cache based on Team: None & Version: 6.0.5  -------------")
+        Team = cache.Team_None
+        ServerName = "testCache0141"
+        Password = "Qwer1234!!"
+        Cache_Version = cache.version_6_0_5
+        WebClientEmail = "test@gmail.com"
+        WebClientPassword = "Qwer1234!!"
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CC.cache(self, Team, ServerName, Password, Cache_Version, WebClientEmail, WebClientPassword)
+
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+    @pytest.mark.regression
+    def test_TC003(self, setup):
+
+        print("****************** Try to go create Cache based on Team: Default & Version: 5.0.7  *********************")
+        # self.logger.info("****Started Home page title test ****")
+        self.driver = setup
+        ss = SS(self.driver)
+        cache = CreateCache(self.driver)
+
+        print("****************** Try to Test Cluster Login *********************")
+        try:
+            self.login.test_cluster_login(self)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("-------------------- Try to go create cache page ---------------")
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CF.go_createCachePage(self)
+            time.sleep(3)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("---------- Start create Cache based on Team: Default & Version: 5.0.7  -------------")
+        Team = cache.DefaultTeam_cache
+        ServerName = "testCache0141"
+        Password = "Qwer1234!!"
+        Cache_Version = cache.Version_5_0_7
+        WebClientEmail = "test@gmail.com"
+        WebClientPassword = "Qwer1234!!"
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CC.cache(self, Team, ServerName, Password, Cache_Version, WebClientEmail, WebClientPassword)
+
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+    @pytest.mark.regression
+    def test_TC004(self, setup):
+
+        print("****************** Try to go create Cache based on Team: Default & Version: 6.0.5  *********************")
+        # self.logger.info("****Started Home page title test ****")
+        self.driver = setup
+        ss = SS(self.driver)
+        cache = CreateCache(self.driver)
+
+        print("****************** Try to Test Cluster Login *********************")
+        try:
+            self.login.test_cluster_login(self)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("-------------------- Try to go create cache page ---------------")
+
+        try:
+            self.driver.refresh()
+            time.sleep(3)
+            self.CF.go_createCachePage(self)
+            time.sleep(3)
+        except NoSuchElementException as e:
+            print("NoSuchElementException error :\n", e, "\n")
+        except TimeoutException as e:
+            print("TimeoutException error", e)
+        except InvalidSessionIdException as e:
+            print("InvalidSessionIdException", e)
+
+        print("---------- Start create Cache based on Team: Default & Version: 5.0.7  -------------")
+        Team = cache.DefaultTeam_cache
+        ServerName = "testCache0141"
+        Password = "Qwer1234!!"
+        Cache_Version = cache.version_6_0_5
         WebClientEmail = "test@gmail.com"
         WebClientPassword = "Qwer1234!!"
 
